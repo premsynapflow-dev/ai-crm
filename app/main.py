fbe8d62182de72c620896a0226397b52c46201bd
premneuronyx-spec <prem.neuronyx@gmail.com>
2026-03-04 13:52:38 +0530

    made changes in main.py for CORS issue, so that external websites can use our API

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 ﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.exc import SQLAlchemyError
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -15,6 +16,14 @@ logger = get_logger(__name__)
 settings = get_settings()
 
 app = FastAPI(title="AI Complaint Intelligence API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(
     SessionMiddleware,
